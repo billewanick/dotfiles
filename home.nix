3e5a9751cbc40098ff62a0d670a2e7b5c4e9b73c
Bill Ewanick <bill@ewanick.com>
2022-07-01 00:26:34 -0400

    Add a variable to zsh to fix annoying error

--- home.nix
+++ home.nix
@@ -321,6 +321,9 @@
             # Stay in the current shell
           fi
         fi
+
+        # Fix for a Nix perl issue? https://github.com/NixOS/nixpkgs/issues/8398
+        export LC_ALL=en_US.UTF-8  
       '';
     };
   };
