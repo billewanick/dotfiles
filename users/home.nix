d251d7928924eca11978acd6ab5e85fad523cb4c
Bill Ewanick <bill@ewanick.com>
2022-07-17 22:23:17 -0400

    Add picom for window opacity, etc

--- users/home.nix
+++ users/home.nix
@@ -80,6 +80,20 @@ in
     theme = "Arc-Dark";
   };
 
+  services.picom = {
+    enable = true;
+    activeOpacity = 1.0;
+    inactiveOpacity = 0.8;
+    backend = "glx";
+    fade = true;
+    fadeDelta = 5;
+    opacityRules = [
+      "100:name *= 'i3lock'"
+    ];
+    shadow = true;
+    shadowOpacity = 0.75;
+  };
+
   # Background image
   home.file.".background-image".source = ../background.jpg;
 
