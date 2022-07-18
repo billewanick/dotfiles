1b79b725cdc685600a18c924cc445acaa4827061
Bill Ewanick <bill@ewanick.com>
2022-07-17 21:46:05 -0400

    Move /workspace function into zsh functions file

--- users/modules/cli/default.nix
+++ users/modules/cli/default.nix
@@ -110,16 +110,7 @@
     };
 
     initExtra = ''
-      # If to always go to home, unless nix-shell stay in folder
-      # -z is true if length of string is zero.
-      if [ -z $IN_NIX_SHELL ]; then
-        # New terminal, go to home
-        cd /workspace
-      else
-        if [ $IN_NIX_SHELL = "impure" ]; then
-          # Stay in the current shell
-        fi
-      fi
+
     '';
 
     initExtraBeforeCompInit = ''
