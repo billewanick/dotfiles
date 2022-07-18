b23d3a12ab0f39453cbda6a15d3d4e8b51c93222
Bill Ewanick <bill@ewanick.com>
2022-07-17 20:55:50 -0400

    Add home-manager as a NixOS module for testing purposes

--- system/flake.nix
+++ system/flake.nix
@@ -1,9 +1,36 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-    };
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nur = {
+    url = "github:nix-community/NUR";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
+
+  outputs = inputs@{ nixpkgs, home-manager, nur, ... }:
+    let
+      overlays = [
+        nur.overlay
+      ];
+
+      pkgs = nixpkgs.legacyPackages.${system};
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            # home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.bill = import ../users/home.nix;
+            home-manager.extraSpecialArgs = { inherit overlays; };
+          }
+        ];
+      };
+    };
 }
